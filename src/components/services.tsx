b82e83c3ea6d31d073da324851fdc61fe6e029f9
Shaked Aharon <srek123@gmail.com>
2024-10-09 10:18:10 +0300

    Added . to make icons in services with relative path

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -2,32 +2,32 @@ const services = [
     {
         name: 'Web Development',
         description: 'Building responsive and modern web applications using the latest technologies like Vanila, React, Angular, WordPress and more.',
-        icon: '/public/web-icon.svg'
+        icon: './public/web-icon.svg'
     },
     {
         name: 'Mobile Development',
         description: 'Creating mobile applications for iOS and Android platforms with React Native and Ionic frameworks.',
-        icon: '/public/mobile-icon.svg'
+        icon: './public/mobile-icon.svg'
     },
     {
         name: 'API Development',
         description: 'Designing and building secure RESTful and GraphQL APIs to support front-end and mobile applications.',
-        icon: '/public/api-icon.svg'
+        icon: './public/api-icon.svg'
     },
     {
         name: 'DevOps',
         description: 'Setting up cloud infrastructure using AWS, Azure, or Google Cloud for scalable and secure deployments. This includes configuring CI/CD pipelines to automate deployments and manage applications efficiently.',
-        icon: '/public/devops-icon.svg'
+        icon: './public/devops-icon.svg'
     },
     {
         name: 'Plugin and Extension Development',
         description: 'Developing custom plugins or extensions for platforms like WordPress, Shopify, or Trello to extend their capabilities according to client requirements. This includes API integrations, custom functionalities, and providing user-friendly admin interfaces for ease of use.',
-        icon: '/public/plugin-icon.svg'
+        icon: './public/plugin-icon.svg'
     },
     {
         name: 'Automation and Scripting Solutions',
         description: 'Automating repetitive tasks and processes using scripting languages like Python or JavaScript. This includes building bots, data scraping tools, and automation scripts for infrastructure management and deployment.',
-        icon: '/public/automation-icon.svg'
+        icon: './public/automation-icon.svg'
     },
 
 ]
