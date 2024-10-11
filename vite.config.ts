af124dca979d4d3d28c0d0dbb4e5bbfcfac15652
Shaked Aharon <srek123@gmail.com>
2024-10-11 11:55:11 +0300

    Made vite build to include CNAME and also output dir as docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './'
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'CNAME', // Path to your CNAME file in the project root
+          dest: '.'     // Destination in the build folder
+        }
+      ]
+    })
+  ],
+  base: './',
+  build: {
+    outDir: 'docs',
+  }
 })
